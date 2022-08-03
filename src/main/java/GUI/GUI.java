582d948378f30968390c34bb30b5c8a310bc605f
khanmt <khanmt@UK-LC71HM33>
2022-08-03 11:32:48 +0100

    OTP GENERATOR TOOL

---
+++ src/main/java/GUI/GUI.java
@@ -0,0 +1,91 @@
+package GUI;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.jboss.aerogear.security.otp.Totp;
+
+public class GUI implements ActionListener
+{
+
+    private JTextField otpLabel;
+    private JLabel secretLabel;
+    private JFrame frame;
+    private JPanel panel;
+    private JButton generateOtpButton;
+    private JButton copyOtp;
+    private JTextField textField;
+
+    public GUI()
+    {
+        frame = new JFrame();
+        panel = new JPanel();
+        textField = new JTextField(20);
+        generateOtpButton = new JButton("GENERATE OTP");
+        otpLabel = new JTextField("OTP CODE");
+        secretLabel = new JLabel("Enter secret");
+        copyOtp = new JButton(new AbstractAction("COPY CODE")
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                StringSelection copiedString = new StringSelection(otpLabel.getText());
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clipboard.setContents(copiedString, null);
+            }
+        });
+
+
+        panel.setBorder(BorderFactory.createEmptyBorder(70,70,10,70));
+        panel.setLayout(new GridLayout(0, 1));
+
+        secretLabel.setBounds(10,50,80,25);
+        panel.add(secretLabel);
+
+        textField.setBounds(100, 20 ,165, 25);
+        panel.add(textField);
+
+        panel.add(generateOtpButton);
+
+        panel.add(otpLabel);
+
+        panel.add(copyOtp);
+
+        generateOtpButton.addActionListener(this);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("OTP Generator");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        String otpKeyStr = textField.getText();
+
+        Totp totp = new Totp(otpKeyStr);
+        String twoFactorCode = totp.now();
+
+        otpLabel.setText(twoFactorCode);
+    }
+
+
+    public void copOtp(ActionEvent e)
+    {
+        StringSelection copiedString = new StringSelection(otpLabel.getText());
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(copiedString, null);
+    }
+
+}
